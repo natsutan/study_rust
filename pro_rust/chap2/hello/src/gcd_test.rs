85d99309473d19df19efee9fc04898e5f0c73fea
natsutan <YRW03704@nifty.com>
2018-08-27 23:04:43 +0900

    work

---
+++ pro_rust/chap2/hello/src/gcd_test.rs
@@ -0,0 +1,5 @@
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14,15),1);
+    
+}
---
+++ pro_rust/chap2/hello/src/main.rs
@@ -0,0 +1,25 @@
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14,15),1);    
+}
+
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(m != 0 && m != 0);
+    while m != 0{
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }    
+    n
+}
+
+
+fn main() {
+    println!("Hello, world!");
+    //println!("%d", gcd(10, 200));
+}
+
